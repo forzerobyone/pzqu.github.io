54bcb7cdb088847ce6d2129d4eb942e43764429a
pzqu <pzqu@example.com>
2019-05-01 17:29:08 +0800

    Site updated: 2019-05-01 17:29:08

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 10:24:37 GMT+0800 (China Standard Time)
+// build time:Wed May 01 2019 17:29:06 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
